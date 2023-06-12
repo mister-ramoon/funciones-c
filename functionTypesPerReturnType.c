aaf2ac2b50c18f9f1cc4ef701cee0088e7754faf
ramonruizdev <ramonruizrc@gmail.com>
2023-06-12 06:46:30 -0600

    Tipos de retorno en funciones

---
+++ functionTypesPerReturnType.c
@@ -0,0 +1,23 @@
+// Funcioes sin argumentos y sin valor de retorno
+void functionName();
+functionName();
+
+vioid functionName() {
+    // Logic
+}
+
+// Funciones con argumentos y sin valor de retorno
+void fuctionNameArg(int arg1, float arg2);
+
+functionNameArg(1, 2.0);
+
+void functionNameArg(int arg1, float arg2) {
+    // Logic
+}
+
+// Funciones sin argumentos y con valor de retorno
+float functionNameRet();
+
+// Funciones con argumentos y con valor de retorno
+float functionNameArgRet(int arg1, float arg2);
+
