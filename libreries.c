298e0bf4f32e907b8dd8dbe7458f0215ef7deed6
ramonruiz <jruiz@lightia.mx>
2023-06-16 13:02:45 -0600

    Principales Bibliotecas de C

---
+++ libreries.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <signal.h>
+#include <locale.h>
+#include <errno.h>
+#include <assert.h>
